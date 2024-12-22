5cd16095613c3765b9a738f8e07f2b99f9e4847c
Nishant Patil <pnishant2323@gmail.com>
2024-12-22 09:56:27 +0530

    change

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -9,17 +9,18 @@ interface MapTypes {
 }
 
 const Map: React.FunctionComponent<MapTypes> = ({ lat, lng, zoom = 8 }) => {
-    return (
-        <MapContainer center={[lat, lng]} zoom={zoom} scrollWheelZoom={false} style={{ width: '100%', height: "200px" }}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[lat, lng]}>
-                {/* <Popup >Popup is here</Popup> */}
-            </Marker>
-        </MapContainer>
-    )
+    if (typeof window !== "undefined")
+        return (
+            <MapContainer center={[lat, lng]} zoom={zoom} scrollWheelZoom={false} style={{ width: '100%', height: "200px" }}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[lat, lng]}>
+                    {/* <Popup >Popup is here</Popup> */}
+                </Marker>
+            </MapContainer>
+        )
 }
 
 export default Map
